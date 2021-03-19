3f74e816f64ec7f86670a6fe338c2a47eeab3dd0
Jacob Bradshaw <jakebradshaw123@gmail.com>
2021-03-19 15:19:24 -0700

    sdfsdf

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SnowApp from './SnowApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SnowApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,14 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  getUser: function() {
+    return axios.get("/user/");
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+
+  saveUser: function(userData) {
+    return axios.post("/user/", userData);
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/test/" + id);
+  checkUser: function(userData) {
+    return axios.post("/user/login", userData);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
 };
